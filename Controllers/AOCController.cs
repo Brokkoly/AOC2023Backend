48b69635dbce28cef7377fb74b1374a73f103523
Brendan Krull <Brendan Krull@DESKTOP-I13UFFB>
2023-11-30 14:02:55 -0600

    Add project files.

---
+++ Controllers/AOCController.cs
@@ -0,0 +1,17 @@
+﻿using AOC2023Backend.Days;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AOC2023Backend.Controllers
+{
+    [ApiController]
+    [Route("aoc")]
+    public class AOCController : ControllerBase
+    {
+        [Route("/DayOne")]
+        [HttpPost]
+        public IActionResult DayOne ([FromBody] string inputString)
+        {
+            return new OkObjectResult((new DayOne()).PartOneAndTwo(inputString));
+        }
+    }
+}
---
+++ Days/Day.cs
@@ -0,0 +1,37 @@
+﻿namespace AOC2023Backend.Days
+{
+    public abstract class Day
+    {
+        public Answer PartOneAndTwo(string input)
+        {
+            return new Answer()
+            {
+                PartOneAnswer = PartOne(input),
+                PartTwoAnswer = PartTwo(input)
+            };
+        }
+
+        public abstract string PartOne(string input);
+
+        public abstract string PartTwo(string input);
+
+        public List<string> ParseInput(string input)
+        {
+            return input.Split('\n').ToList();
+        }
+        public List<List<char>> ParseInput2D(string input)
+        {
+            var lines = input.Split('\n').ToList();
+            return lines.Select(line =>
+            {
+                return line.ToList();
+            }).ToList();
+        }
+    }
+
+    public class Answer
+    {
+        public string PartOneAnswer { get; set; }
+        public string PartTwoAnswer { get; set; }
+    }
+}
