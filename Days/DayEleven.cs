64b340b0aed5abba2796b322899b8475beec864e
Brendan Krull <brendankrull@gmail.com>
2023-12-11 16:25:08 -0600

    did day10 and day11

--- Days/DayEleven.cs
+++ Days/DayEleven.cs
@@ -4,12 +4,199 @@
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var rows = ParseInput2D(input);
+            var galaxies = new List<Galaxy>();
+            for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+            {
+                var noGalaxies = !rows[yIndex].Contains('#');
+                if (noGalaxies)
+                {
+                    var newRow = new List<char>();
+                    for (int i = 0; i < rows[yIndex].Count; i++)
+                    {
+                        newRow.Add('.');
+                    }
+                    rows.Insert(yIndex, newRow);
+                    yIndex++;
+                }
+            }
+            for (int xIndex = 0; xIndex < rows[0].Count; xIndex++)
+            {
+                var noGalaxies = true;
+                for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+                {
+                    if (rows[yIndex][xIndex] == '#')
+                    {
+                        noGalaxies = false;
+                    }
+                }
+                if (noGalaxies)
+                {
+                    foreach (var row in rows)
+                    {
+                        row.Insert(xIndex, '.');
+                    }
+                }
+                xIndex++;
+            }
+            for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+            {
+                for (int xIndex = 0; xIndex < rows[yIndex].Count; xIndex++)
+                {
+                    if (rows[yIndex][xIndex] == '#')
+                    {
+                        galaxies.Add(new Galaxy(xIndex, yIndex));
+                    }
+
+                }
+            }
+            var galaxyDistances = new Dictionary<(string, string), int>();
+            for (int i = 0; i < galaxies.Count; i++)
+            {
+                for (int j = 1; j < galaxies.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+
+
+                    }
+                    var iKeyBigger = galaxies[i].Coords.CompareTo(galaxies[j].Coords) == 1;
+                    var key = (iKeyBigger ? galaxies[i].Coords : galaxies[j].Coords, !iKeyBigger ? galaxies[i].Coords : galaxies[j].Coords);
+                    if (galaxyDistances.ContainsKey(key))
+                    {
+                        continue;
+                    }
+
+                    var distance = Math.Abs(galaxies[i].YIndex - galaxies[j].YIndex) + Math.Abs(galaxies[j].XIndex - galaxies[i].XIndex);// + 1;
+                    galaxyDistances.Add(key, distance);
+                }
+            }
+
+            var distanceSum = 0;
+            foreach (var distance in galaxyDistances.Values)
+            {
+                distanceSum += distance;
+            }
+            return distanceSum.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var multiplier = 1000000;
+            var rows = ParseInput2D(input);
+            var galaxies = new List<Galaxy>();
+            var yExtras = new List<int>();
+            var xExtras = new List<int>();
+            //Expand number of rows and add extra if needed
+            for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+            {
+                yExtras.Add(1);
+                var noGalaxies = !rows[yIndex].Contains('#');
+                if (noGalaxies)
+                {
+                    var newRow = new List<char>();
+                    for (int i = 0; i < rows[yIndex].Count; i++)
+                    {
+                        newRow.Add('.');
+                    }
+                    rows.Insert(yIndex, newRow);
+                    yIndex++;
+                    yExtras.Add(multiplier - 1);
+                }
+            }
+            //Expand columns and add extra as needed
+            for (int xIndex = 0; xIndex < rows[0].Count; xIndex++)
+            {
+                xExtras.Add(1);
+                var noGalaxies = true;
+                for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+                {
+                    if (rows[yIndex][xIndex] == '#')
+                    {
+                        noGalaxies = false;
+                        break;
+                    }
+                }
+                if (noGalaxies)
+                {
+                    foreach (var row in rows)
+                    {
+                        row.Insert(xIndex, '.');
+                    }
+                    xIndex++;
+                    xExtras.Add(multiplier - 1);
+                }
+            }
+            //Check For Galaxies with correct coordinates
+            for (int yIndex = 0; yIndex < rows.Count; yIndex++)
+            {
+                for (int xIndex = 0; xIndex < rows[yIndex].Count; xIndex++)
+                {
+                    if (rows[yIndex][xIndex] == '#')
+                    {
+                        galaxies.Add(new Galaxy(xIndex, yIndex));
+                    }
+
+                }
+            }
+            //Use dictionary of tuples to avoid running twice on pairs
+            var galaxyDistances = new Dictionary<(string, string), long>();
+            for (int i = 0; i < galaxies.Count; i++)
+            {
+                for (int j = 1; j < galaxies.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+
+
+                    }
+                    var iKeyBigger = galaxies[i].Coords.CompareTo(galaxies[j].Coords) == 1;
+                    var key = (iKeyBigger ? galaxies[i].Coords : galaxies[j].Coords, !iKeyBigger ? galaxies[i].Coords : galaxies[j].Coords);
+                    if (galaxyDistances.ContainsKey(key))
+                    {
+                        continue;
+                    }
+
+                    var distance = CalculateDistance(galaxies[i], galaxies[j], xExtras, yExtras);
+                    galaxyDistances.Add(key, distance);
+                }
+            }
+
+            long distanceSum = 0;
+            foreach (var distance in galaxyDistances.Values)
+            {
+                distanceSum += distance;
+            }
+            return distanceSum.ToString();
+        }
+        public long CalculateDistance(Galaxy galaxy1, Galaxy galaxy2, List<int> xCosts, List<int> yCosts)
+        {
+
+            long distance = 0;
+            for (int i = galaxy1.XIndex; galaxy2.XIndex >= galaxy1.XIndex ? i < galaxy2.XIndex : i > galaxy2.XIndex; i += galaxy2.XIndex >= galaxy1.XIndex ? 1 : -1)
+            {
+                distance += xCosts[i];
+            }
+            for (int i = galaxy1.YIndex; galaxy2.YIndex >= galaxy1.YIndex ? i < galaxy2.YIndex : i > galaxy2.YIndex; i += galaxy2.YIndex >= galaxy1.YIndex ? 1 : -1)
+            {
+                distance += yCosts[i];
+            }
+
+            return distance;
+        }
+    }
+
+    public class Galaxy
+    {
+        public int XIndex { get; set; }
+        public int YIndex { get; set; }
+        public string Coords { get => string.Join(',', new List<int> { XIndex, YIndex }); }
+        public Galaxy(int x, int y)
+        {
+            XIndex = x;
+            YIndex = y;
         }
     }
 }
--- Days/DayTen.cs
+++ Days/DayTen.cs
@@ -1,15 +1,237 @@
-﻿namespace AOC2023Backend.Days
+﻿using AOC2023Backend.DataTypes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AOC2023Backend.Days
 {
     public class DayTen : Day
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var pipeMatrix = new Matrix<char>(ParseInput2D(input));
+
+            MatrixNode<char>? startNode = null;
+            foreach (var row in pipeMatrix.Values)
+            {
+                var node = row.Find(node => node.Value == 'S');
+                if (node != null)
+                {
+                    startNode = node;
+                    break;
+                }
+            }
+            if (startNode == null)
+            {
+                throw new Exception("Didn't Find Start");
+            }
+
+            var adjacentPipes = new List<MatrixNode<char>>();
+
+            var left = startNode.GetFromDirection(Direction.Left);
+            var right = startNode.GetFromDirection(Direction.Right);
+            var up = startNode.GetFromDirection(Direction.Up);
+            var down = startNode.GetFromDirection(Direction.Down);
+
+            if (left != null && PipeNode.ShapeToDir(left.Value).Contains(Direction.Right))
+            {
+                adjacentPipes.Add(left);
+            }
+            if (right != null && PipeNode.ShapeToDir(right.Value).Contains(Direction.Left))
+            {
+                adjacentPipes.Add(right);
+            }
+            if (up != null && PipeNode.ShapeToDir(up.Value).Contains(Direction.Down))
+            {
+                adjacentPipes.Add(up);
+            }
+            if (down != null && PipeNode.ShapeToDir(down.Value).Contains(Direction.Up))
+            {
+                adjacentPipes.Add(down);
+            }
+            var distanceFromStart = 1;
+            var currentNode = adjacentPipes[0];
+            var previousNode = startNode;
+            while (currentNode.Value != 'S')
+            {
+                var nextNode = GetNextPipeNode(previousNode, currentNode);
+                previousNode = currentNode;
+                currentNode = nextNode;
+                distanceFromStart++;
+            }
+
+            return (distanceFromStart / 2).ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var pipeInput = ParseInput2D(input).Select(x => x.Select(inputChar => new PipeNode(inputChar)).ToList()).ToList();
+            var pipeMatrix = new Matrix<PipeNode>(pipeInput);
+
+            MatrixNode<PipeNode>? startNode = null;
+            foreach (var row in pipeMatrix.Values)
+            {
+                var node = row.Find(node => node.Value.IsStart);
+                if (node != null)
+                {
+                    startNode = node;
+                    break;
+                }
+            }
+            if (startNode == null)
+            {
+                throw new Exception("Didn't Find Start");
+            }
+
+            var adjacentPipes = new List<MatrixNode<PipeNode>>();
+
+            var left = startNode.GetFromDirection(Direction.Left);
+            var right = startNode.GetFromDirection(Direction.Right);
+            var up = startNode.GetFromDirection(Direction.Up);
+            var down = startNode.GetFromDirection(Direction.Down);
+
+            var possiblePipeShapes = new List<char> { 'J', 'L', 'F', '|', '-', '7' };
+
+            if (left != null && PipeNode.ShapeToDir(left.Value.PipeShape).Contains(Direction.Right))
+            {
+                adjacentPipes.Add(left);
+                possiblePipeShapes.Remove('L');
+                possiblePipeShapes.Remove('F');
+                possiblePipeShapes.Remove('|');
+            }
+            if (right != null && PipeNode.ShapeToDir(right.Value.PipeShape).Contains(Direction.Left))
+            {
+                adjacentPipes.Add(right);
+                possiblePipeShapes.Remove('J');
+                possiblePipeShapes.Remove('7');
+                possiblePipeShapes.Remove('|');
+            }
+            if (up != null && PipeNode.ShapeToDir(up.Value.PipeShape).Contains(Direction.Down))
+            {
+                adjacentPipes.Add(up);
+                possiblePipeShapes.Remove('-');
+                possiblePipeShapes.Remove('7');
+                possiblePipeShapes.Remove('F');
+            }
+            if (down != null && PipeNode.ShapeToDir(down.Value.PipeShape).Contains(Direction.Up))
+            {
+                adjacentPipes.Add(down);
+                possiblePipeShapes.Remove('L');
+                possiblePipeShapes.Remove('J');
+                possiblePipeShapes.Remove('-');
+            }
+            if (possiblePipeShapes.Count == 1)
+            {
+                startNode.Value.PipeShape = possiblePipeShapes[0];
+            }
+            var distanceFromStart = 1;
+            var currentNode = adjacentPipes[0];
+            currentNode.Value.InLoop = true;
+            var previousNode = startNode;
+            while (!currentNode.Value.IsStart)
+            {
+                var nextNode = GetNextPipeNode(previousNode, currentNode);
+                nextNode.Value.InLoop = true;
+                previousNode = currentNode;
+                currentNode = nextNode;
+                distanceFromStart++;
+            }
+            var upChars = new List<char> { 'J', 'L', '|' };
+            var numInside = 0;
+            foreach (var row in pipeMatrix.Values)
+            {
+                var inside = false;
+                for (int i = 0; i < row.Count; i++)
+                {
+                    if (row[i].Value.InLoop)
+                    {
+                        if (upChars.Contains(row[i].Value.PipeShape))
+                        {
+                            inside = !inside;
+                        }
+                    }
+                    else
+                    {
+                        if (inside)
+                        {
+                            row[i].Value.PipeShape = 'I';
+                            numInside++;
+                        }
+                        else
+                        {
+                            row[i].Value.PipeShape = 'O';
+                        }
+                    }
+
+                }
+            }
+            var rowStrings = pipeMatrix.Values.Select(row =>
+            {
+                var newRow = String.Join("", row.Select(x => x.Value.PipeShape).ToList());
+                return newRow;
+            });
+            Console.WriteLine(String.Join('\n', rowStrings));
+
+
+            return numInside.ToString();
+
+
         }
+
+        public MatrixNode<char> GetNextPipeNode(MatrixNode<char> previousNode, MatrixNode<char> CurrentNode)
+        {
+            var dirs = PipeNode.ShapeToDir(CurrentNode.Value);
+            var nextNode = dirs.Select(dir => CurrentNode.GetFromDirection(dir)).Where(node => node != null && !node.SameNode(previousNode)).ToList();
+            return nextNode[0] ?? throw new Exception("Couldn't find next pipe");
+        }
+
+        public MatrixNode<PipeNode> GetNextPipeNode(MatrixNode<PipeNode> previousNode, MatrixNode<PipeNode> CurrentNode)
+        {
+            var dirs = PipeNode.ShapeToDir(CurrentNode.Value.PipeShape);
+            var nextNode = dirs.Select(dir => CurrentNode.GetFromDirection(dir)).Where(node => node != null && !node.SameNode(previousNode)).ToList();
+            return nextNode[0] ?? throw new Exception("Couldn't find next pipe");
+        }
+
+    }
+
+    public interface IPipeNode
+    {
+        public bool IsStart { get; set; }
+        //public int X { get; set; }
+        //public int Y { get; set; }
+        public List<IPipeNode> ConnectedNodes { get; set; }
+        public List<IPipeNode> GetConnectedNodes();
+    }
+
+    public class PipeNode
+    {
+        public static List<Direction> ShapeToDir(char shape)
+        {
+            switch (shape)
+            {
+                case '|': return new List<Direction> { Direction.Up, Direction.Down };
+                case '-': return new List<Direction> { Direction.Left, Direction.Right };
+                case 'L': return new List<Direction> { Direction.Up, Direction.Right };
+                case 'J': return new List<Direction> { Direction.Left, Direction.Up };
+                case '7': return new List<Direction> { Direction.Left, Direction.Down };
+                case 'F': return new List<Direction> { Direction.Down, Direction.Right };
+                default:
+                    throw new Exception("Ceci n'est pas une pipe");
+            }
+        }
+
+        public PipeNode(char inputChar)
+        {
+            PipeShape = inputChar;
+            if (PipeShape == 'S')
+            {
+                IsStart = true;
+                InLoop = true;
+            }
+        }
+
+        public char PipeShape { get; set; }
+        public bool IsStart { get; set; } = false;
+
+        public bool InLoop { get; set; } = false;
+
     }
 }
