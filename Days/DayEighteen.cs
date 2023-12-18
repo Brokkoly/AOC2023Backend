5a3484e2c3e0f46bd4143939cd25d7ac5337b9d9
Brendan Krull <brendankrull@gmail.com>
2023-12-18 15:52:54 -0600

    2 off on day 17

--- Days/DayEighteen.cs
+++ Days/DayEighteen.cs
@@ -1,15 +1,263 @@
-﻿namespace AOC2023Backend.Days
+﻿using AOC2023Backend.DataTypes;
+
+namespace AOC2023Backend.Days
 {
     public class DayEighteen : Day
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var actions = ParseInput(input).Select(x => DiggerAction.DiggerActionFactory(x)).ToList();
+
+
+            long currentX = 0;
+            long currentY = 0;
+            long currentSum = 0;
+            var firstVertex = new Vertex() { XIndex = currentX, YIndex = currentY };
+            var vertices = new List<Vertex> { firstVertex };
+            var previousVertex = firstVertex;
+            for (var actionIndex = 0; actionIndex < actions.Count; actionIndex++)
+            {
+                var action = actions[actionIndex];
+                //var nextAction = actions[actionIndex + 1];
+                switch (action.Direction)
+                {
+                    case Direction.Up:
+                        currentY += action.Distance;
+                        break;
+                    case Direction.Down:
+                        currentY -= action.Distance;
+                        break;
+                    case Direction.Left:
+                        currentX -= action.Distance;
+                        break;
+                    case Direction.Right:
+                        currentX += action.Distance;
+                        break;
+                }
+                var newVertex = new Vertex()
+                {
+                    XIndex = currentX,
+                    YIndex = currentY
+                };
+                currentSum += (previousVertex.XIndex * newVertex.YIndex - previousVertex.YIndex * newVertex.XIndex);
+                previousVertex = newVertex;
+                vertices.Add(newVertex);
+            }
+            currentSum += (previousVertex.XIndex * firstVertex.YIndex - previousVertex.YIndex * firstVertex.XIndex);
+
+            long area = Math.Abs(currentSum) / 2;
+
+            long perimeter = 1;
+            foreach (var action in actions)
+            {
+                perimeter += action.Distance;
+            }
+            area += perimeter / 2 + 1;
+
+            return area.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+
+            var actions = ParseInput(input).Select(x => DiggerAction.DiggerActionFactoryPart2(x)).ToList();
+            long currentX = 0;
+            long currentY = 0;
+            long currentSum = 0;
+            var firstVertex = new Vertex() { XIndex = currentX, YIndex = currentY };
+            var vertices = new List<Vertex> { firstVertex };
+            var previousVertex = firstVertex;
+            for (var actionIndex = 0; actionIndex < actions.Count; actionIndex++)
+            {
+                var action = actions[actionIndex];
+                //var nextAction = actions[actionIndex + 1];
+                switch (action.Direction)
+                {
+                    case Direction.Up:
+                        currentY += action.Distance;
+                        break;
+                    case Direction.Down:
+                        currentY -= action.Distance;
+                        break;
+                    case Direction.Left:
+                        currentX -= action.Distance;
+                        break;
+                    case Direction.Right:
+                        currentX += action.Distance;
+                        break;
+                }
+                var newVertex = new Vertex()
+                {
+                    XIndex = currentX,
+                    YIndex = currentY
+                };
+                currentSum += (previousVertex.XIndex * newVertex.YIndex - previousVertex.YIndex * newVertex.XIndex);
+                previousVertex = newVertex;
+                vertices.Add(newVertex);
+            }
+            currentSum += (previousVertex.XIndex * firstVertex.YIndex - previousVertex.YIndex * firstVertex.XIndex);
+
+            long area = Math.Abs(currentSum) / 2;
+
+            long perimeter = 1;
+            foreach (var action in actions)
+            {
+                perimeter += action.Distance;
+            }
+            area += perimeter / 2 + 1;
+
+            return area.ToString();
+        }
+    }
+
+    public class DiggerAction
+    {
+        public Direction Direction { get; set; }
+        public long Distance { get; set; }
+        public char DirectionChar { get; set; }
+        public static DiggerAction DiggerActionFactory(string input)
+        {
+            var splits = input.Split(' ');
+            var distance = long.Parse(splits[1]);
+            Direction dir;
+            switch (splits[0])
+            {
+                case "R": dir = Direction.Right; break;
+                case "U": dir = Direction.Up; break;
+                case "L": dir = Direction.Left; break;
+                case "D": dir = Direction.Down; break;
+                default:
+                    throw new Exception();
+            }
+            var color = long.Parse(splits[2][2..(splits[2].Length - 1)], System.Globalization.NumberStyles.HexNumber);
+            return new DiggerAction(dir, distance, splits[0][0]);
+        }
+
+        public static DiggerAction DiggerActionFactoryPart2(string input)
+        {
+            var splits = input.Split(' ');
+            var color = long.Parse(splits[1]);
+            Direction dir;
+
+            var hex = splits[2][2..(splits[2].Length - 1)];
+
+            var dirChar = '0';
+
+            switch (hex.Last())
+            {
+                case '0':
+                    dir = Direction.Right;
+                    dirChar = 'R';
+                    break;
+                case '3':
+                    dir = Direction.Up;
+                    dirChar = 'U';
+                    break;
+                case '2':
+                    dir = Direction.Left;
+                    dirChar = 'L';
+                    break;
+                case '1':
+                    dir = Direction.Down;
+                    dirChar = 'D';
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            var distance = long.Parse(hex[..(hex.Length - 1)], System.Globalization.NumberStyles.HexNumber);
+            return new DiggerAction(dir, distance, dirChar);
+        }
+
+        public DiggerAction(Direction direction, long distance, char directionChar)
+        {
+            Direction = direction;
+            Distance = distance;
+            DirectionChar = directionChar;
+        }
+
+
+
+    }
+    public class Vertex
+    {
+        public long XIndex;
+        public long YIndex;
+    }
+    public static class Extension
+    {
+        public static void DigOut(this Matrix<char> myMatrix, List<DiggerAction> actions, int startX, int startY)
+        {
+            var firstNode = myMatrix.GetNode(startX, startY);
+            if (firstNode == null)
+            {
+                throw new Exception();
+            }
+            var nextNode = firstNode;
+            foreach (var action in actions)
+            {
+                var distanceLeft = action.Distance;
+                //For later inside checking
+                if (action.Direction == Direction.Up || action.Direction == Direction.Down)
+                {
+                    nextNode.Value = action.DirectionChar;
+                }
+
+                while (distanceLeft > 0)
+                {
+                    nextNode = nextNode.GetFromDirection(action.Direction);
+                    distanceLeft--;
+                    if (nextNode == null)
+                    {
+                        throw new Exception();
+                    }
+                    nextNode.Value = action.DirectionChar;
+                }
+            }
+        }
+
+        public static long CalculateTrenchSize(this Matrix<char> myMatrix)
+        {
+            var wholeSize = 0;
+            bool inside = false;
+            char lastUpDown = '0';
+            foreach (var row in myMatrix.Values)
+            {
+                inside = false;
+                lastUpDown = '0';
+                foreach (var node in row)
+                {
+                    if (node.Value == 'U' || node.Value == 'D')
+                    {
+                        if (node.Value != lastUpDown)
+                        {
+                            inside = !inside;
+                            lastUpDown = node.Value;
+                        }
+                    }
+                    else if (inside && node.Value != 'L' && node.Value != 'R')
+                    {
+                        node.Value = '#';
+                    }
+                }
+            }
+            foreach (var row in myMatrix.Values)
+            {
+                foreach (var node in row)
+                {
+                    switch (node.Value)
+                    {
+                        case 'U':
+                        case 'D':
+                        case 'L':
+                        case 'R':
+                        case '#':
+                            wholeSize++;
+                            break;
+                    }
+                }
+            }
+            return wholeSize;
         }
     }
 }
--- Days/DaySeventeen.cs
+++ Days/DaySeventeen.cs
@@ -1,15 +1,234 @@
-﻿namespace AOC2023Backend.Days
+﻿using AOC2023Backend.DataTypes;
+
+namespace AOC2023Backend.Days
 {
     public class DaySeventeen : Day
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var values = ParseInput2D(input).Select(x => x.Select(y => int.Parse(y.ToString())).ToList()).ToList();
+
+            var djikstraMatrix = DjikstraMatrix.DjikstraMatrixFactory(values);
+
+            var shortestPath = djikstraMatrix.FindShortest();
+            return shortestPath.ToString();
         }
 
         public override string PartTwo(string input)
         {
             throw new NotImplementedException();
         }
+    }
+
+    public class DjikstraValue
+    {
+        public Dictionary<Direction, List<(MatrixNode<DjikstraValue>, int)>> AdjacenciesFromDirection { get; set; } = new();
+        public int Weight { get; set; }
+
+        //The key here is the direction of travel into the node
+        public Dictionary<Direction, bool> VisitedFromDir { get; set; } = new();
+        //The key here is the direction of travel into the node
+        public Dictionary<Direction, int> DistanceFromStartFromDirection { get; set; } = new();
+        public DjikstraValue(int weight)
+        {
+            Weight = weight;
+            DistanceFromStartFromDirection[Direction.Up] = int.MaxValue;
+            DistanceFromStartFromDirection[Direction.Right] = int.MaxValue;
+            VisitedFromDir[Direction.Down] = false;
+            VisitedFromDir[Direction.Up] = false;
+            VisitedFromDir[Direction.Left] = false;
+            VisitedFromDir[Direction.Right] = false;
+        }
+        public int GetDistanceFromStartFromDirection(Direction direction)
+        {
+            var actualDirection = direction;
+            if (actualDirection == Direction.Down)
+            {
+                actualDirection = Direction.Up;
+            }
+            else if (actualDirection == Direction.Left)
+            {
+                actualDirection = Direction.Right;
+            }
+            return DistanceFromStartFromDirection[actualDirection];
+        }
+
+        public void SetDistanceFromStartFromDirection(Direction direction, int distance)
+        {
+            var actualDirection = direction;
+            if (actualDirection == Direction.Down)
+            {
+                actualDirection = Direction.Up;
+            }
+            else if (actualDirection == Direction.Left)
+            {
+                actualDirection = Direction.Right;
+            }
+            DistanceFromStartFromDirection[actualDirection] = distance;
+        }
+    }
+
+    public class DjikstraMatrix : Matrix<DjikstraValue>
+    {
+        public int MaxCost { get; set; }
+        public static DjikstraMatrix DjikstraMatrixFactory(List<List<int>> input)
+        {
+            var values = input.Select(x => x.Select(y => new DjikstraValue(y)).ToList()).ToList();
+            var matrix = new DjikstraMatrix(values);
+            matrix.Initialize();
+            return matrix;
+        }
+
+        public List<(MatrixNode<DjikstraValue>, Direction)> GetNextNodes(MatrixNode<DjikstraValue> node, Direction inputDir)
+        {
+            var retNodes = new List<(MatrixNode<DjikstraValue>, Direction)>();
+            node.Value.VisitedFromDir[inputDir] = true;
+            var directionDict = new Dictionary<Direction, List<MatrixNode<DjikstraValue>>>();
+            foreach (var dir in new List<Direction> { Direction.Up, Direction.Right, Direction.Down, Direction.Left })
+            {
+                if (dir == inputDir) continue;
+                else if (dir == Direction.Up && inputDir == Direction.Down) continue;
+                else if (dir == Direction.Down && inputDir == Direction.Up) continue;
+                else if (dir == Direction.Left && inputDir == Direction.Right) continue;
+                else if (dir == Direction.Right && inputDir == Direction.Left) continue;
+                directionDict[dir] = new();
+                var node1 = node.GetFromDirection(dir);
+                if (node1 == null)
+                {
+                    continue;
+                }
+                var cost = node.Value.GetDistanceFromStartFromDirection(inputDir);
+                if (cost == int.MaxValue)
+                {
+                    throw new Exception();
+                }
+                cost += node1.Value.Weight;
+                node1.Value.SetDistanceFromStartFromDirection(dir, Math.Min(cost, node1.Value.GetDistanceFromStartFromDirection(dir)));
+                if (!node1.Value.VisitedFromDir[dir])
+                {
+                    directionDict[dir].Add(node1);
+                }
+                var node2 = node1.GetFromDirection(dir);
+                if (node2 == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    cost += node2.Value.Weight;
+                    node2.Value.SetDistanceFromStartFromDirection(dir, Math.Min(cost, node2.Value.GetDistanceFromStartFromDirection(dir)));
+                    if (!node2.Value.VisitedFromDir[dir])
+                    {
+                        directionDict[dir].Add(node2);
+                    }
+                }
+                var node3 = node2.GetFromDirection(dir);
+                if (node3 == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    cost += node3.Value.Weight;
+                    node3.Value.SetDistanceFromStartFromDirection(dir, Math.Min(cost, node3.Value.GetDistanceFromStartFromDirection(dir)));
+                    if (!node3.Value.VisitedFromDir[dir])
+                    {
+                        directionDict[dir].Add(node3);
+                    }
+                }
+            }
+            foreach (var keyValuePair in directionDict)
+            {
+                keyValuePair.Value.ForEach(x =>
+                {
+                    retNodes.Add((x, keyValuePair.Key));
+                });
+            }
+            return retNodes;
+        }
+
+        public int FindShortest()
+        {
+            var firstNode = Values.First().First();
+            var endNode = Values.Last().Last();
+
+            firstNode.Value.SetDistanceFromStartFromDirection(Direction.Up, firstNode.Value.Weight);
+            firstNode.Value.SetDistanceFromStartFromDirection(Direction.Right, firstNode.Value.Weight);
+
+            var possibleNodes = new PriorityQueue<(MatrixNode<DjikstraValue>, Direction), int>();
+            possibleNodes.Enqueue((firstNode, Direction.Right), firstNode.Value.GetDistanceFromStartFromDirection(Direction.Right));
+            possibleNodes.Enqueue((firstNode, Direction.Down), firstNode.Value.GetDistanceFromStartFromDirection(Direction.Down));
+
+            var nextNode = possibleNodes.Peek();
+            while (nextNode.Item1 != endNode && possibleNodes.Count > 0)
+            {
+                nextNode = possibleNodes.Dequeue();
+                //possibleNodes.RemoveAt(possibleNodes.Count - 1);
+
+                var newPossibles = GetNextNodes(nextNode.Item1, nextNode.Item2);
+                foreach (var possibility in newPossibles)
+                {
+                    if (possibility.Item1.Value.GetDistanceFromStartFromDirection(possibility.Item2) < MaxCost)
+                    {
+                        possibleNodes.Enqueue(possibility, possibility.Item1.Value.GetDistanceFromStartFromDirection(possibility.Item2));
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            return nextNode.Item1.Value.GetDistanceFromStartFromDirection(nextNode.Item2);
+        }
+        public void Initialize()
+        {
+            //Values.ForEach(row =>
+            //{
+            //    foreach (var item in row)
+            //    {
+            //        var directionList = new Dictionary<Direction, List<MatrixNode<DjikstraValue>>>();
+            //        foreach (var dir in new List<Direction> { Direction.Up, Direction.Right, Direction.Down, Direction.Left })
+            //        {
+            //            directionList[dir] = new();
+            //            var node1 = item.GetFromDirection(dir);
+            //            if (node1 == null)
+            //            {
+            //                continue;
+            //            }
+            //            else
+            //            {
+            //                directionList[dir].Add(node1);
+            //            }
+            //            var node2 = node1.GetFromDirection(dir);
+            //            if (node2 == null)
+            //            {
+            //                continue;
+            //            }
+            //            else
+            //            {
+            //                directionList[dir].Add(node2);
+            //            }
+            //            var node3 = node2.GetFromDirection(dir);
+            //            if (node3 == null)
+            //            {
+            //                continue;
+            //            }
+            //            else
+            //            {
+            //                directionList[dir].Add(node3);
+            //            }
+            //        }
+
+            //        var nodesFromDirection = new 
+            //    }
+            //});
+        }
+        public DjikstraMatrix(List<List<DjikstraValue>> input, bool reverseYAxis = false) : base(input, reverseYAxis)
+        {
+            MaxCost = (input[0].Count + input.Count) * 10;
+        }
+
+
     }
 }
