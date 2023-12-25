f2c318834c40beaae22630b83ab626aba556da02
Brendan Krull <brendankrull@gmail.com>
2023-12-25 16:20:29 -0600

    update a bit

--- Days/DayTwentyThree.cs
+++ Days/DayTwentyThree.cs
@@ -14,6 +14,30 @@ namespace AOC2023Backend.Days
                 var posAndVel = x.Split(" @ ").Select(y => y.Split(", ").Select(z => int.Parse(z)).ToList()).ToList();
                 return new Hailstone(posAndVel[0][0], posAndVel[0][1], posAndVel[0][2], posAndVel[1][0], posAndVel[1][1], posAndVel[1][2]);
             }).ToList();
+
+            List<(Hailstone, Hailstone)> distinctPairs = new();
+
+            foreach (var hailstone in hailstones)
+            {
+                foreach (var hailstone2 in hailstones)
+                {
+                    if (!distinctPairs.Any(x => (x.Item1 == hailstone && x.Item2 == hailstone2) || (x.Item1 == hailstone2 && x.Item2 == hailstone)))
+                    {
+                        distinctPairs.Add((hailstone, hailstone2));
+                    }
+                }
+            }
+
+            var numInside = 0;
+
+            foreach (var pair in distinctPairs)
+            {
+                var intersectionTimes = pair.Item1.FindIntersectionTimes(pair.Item2);
+
+
+            }
+
+            return numInside.ToString();
         }
 
         public override string PartTwo(string input)
@@ -38,15 +62,26 @@ namespace AOC2023Backend.Days
             DZ = dz;
         }
 
-        public (float, float) FindIntersection(Hailstone otherHailstone)
+        public (float, float) FindIntersectionTimes(Hailstone otherHailstone)
         {
+            var intersectionTimeX = FindSameTime(XIndex, otherHailstone.XIndex, DX, otherHailstone.DX);
 
+            var intersectionTimeY = FindSameTime(YIndex, otherHailstone.YIndex, DY, otherHailstone.DY);
+
+            return (intersectionTimeX, intersectionTimeY);
+        }
+
+        public (float, float) FindPositionAtTime(float time)
+        {
+            return (XIndex + DX * time, YIndex + DY * time);
         }
 
         private float FindSameTime(float p0, float p1, float d0, float d1)
         {
             var result = p0 - p1;
             result /= d1 - d0;
+
+            return result;
         }
     }
 }
